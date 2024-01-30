9d57a2131188bdf8c517aab0b1bd96fa87c1a2a4
Sakib <sakibul706@gmail.com>
2024-01-30 17:54:41 +0600

    ui stuff done kinda

--- main.go
+++ main.go
@@ -8,12 +8,47 @@ import (
 )
 
 type app struct {
-	remaing time.Time
-	queues  chan termbox.Event
+	session uint
+
+	// remaining time
+	current time.Duration
+	timmer  time.Duration
+	// break lenght
+	intermission time.Duration
+
+	queues chan termbox.Event
+	paused bool
+
+	waitForUserInput bool
+	// next is the intermission
+	nextInerm bool
+	ticker    *time.Ticker
 }
 
-func durToString(d time.Duration) string {
-	d = d.Round(time.Second)
+func (a *app) currSession() string {
+	return fmt.Sprintf("Session: %d", a.session)
+}
+func (a *app) nextSession() string {
+	return fmt.Sprintf("Session %d will start soon..", a.session+1)
+}
+
+func (a *app) sessionInc() {
+	a.session++
+}
+func clearT() {
+	if err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
+		panic(err)
+	}
+}
+func flush() {
+	if err := termbox.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+// func (a *app) formatTime() string
+func (a *app) formatDuration() string {
+	d := a.current.Round(time.Second)
 	h := d / time.Hour
 	d -= h * time.Hour
 	m := d / time.Minute
@@ -23,55 +58,101 @@ func durToString(d time.Duration) string {
 		return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
 	}
 	return fmt.Sprintf("%02d:%02d", m, s)
+
+}
+
+func (a *app) pollEvent() {
+	for {
+		a.queues <- termbox.PollEvent()
+	}
+}
+
+func newTimmer() app {
+	dur, _ := time.ParseDuration("2s")
+	return app{
+		session: 1,
+
+		current: dur,
+		timmer:  dur,
+		// intermission: 5 * time.Minute,
+		intermission: 1 * time.Second,
+
+		queues:    make(chan termbox.Event),
+		paused:    false,
+		nextInerm: true,
+		ticker:    time.NewTicker(time.Second),
+	}
 }
 
 func main() {
-	dur, _ := time.ParseDuration("1h10m10s")
 	if err := termbox.Init(); err != nil {
 		panic(err)
 	}
 	defer termbox.Close()
-	queues := make(chan termbox.Event)
-	go func() {
-		for {
-			queues <- termbox.PollEvent()
-		}
-	}()
-
-	paused := false
-	ticker := time.NewTicker(time.Second)
+	a := newTimmer()
+	go a.pollEvent()
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	putText("Starting....", positionMiddle, termbox.ColorDefault+termbox.AttrBold)
+	// putTime(a.formatDuration())
+	flush()
 
 loop:
 	for {
 		select {
-		case ev := <-queues:
-			if ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlC || ev.Ch == 'q' {
+		case ev := <-a.queues:
+			if isQuit(ev) {
 				break loop
 			} else if ev.Key == termbox.KeySpace {
-				paused = !paused
-				if paused {
-					termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-					putTime(durToString(dur))
-					putPaused()
-					termbox.Flush()
-					ticker.Stop()
+				a.paused = !a.paused
+				if a.paused {
+					clearT()
+					if !a.nextInerm {
+						putText(a.nextSession(), positionButtom, termbox.ColorLightBlue+termbox.AttrBold)
+					} else {
+						putText(a.currSession(), positionButtom, termbox.ColorYellow+termbox.AttrBold)
+					}
+					putTime(a.formatDuration())
+					putText("Paused", positionTop, termbox.ColorLightRed+termbox.AttrBold)
+					flush()
+					a.ticker.Stop()
 				} else {
-					ticker.Reset(time.Second)
+					a.ticker.Reset(time.Second)
 				}
 			}
-		case <-ticker.C:
-			termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-			putTime(durToString(dur))
-			dur -= time.Second
-			if dur == 0 {
-				break loop
+
+		case <-a.ticker.C:
+			clearT()
+			putTime(a.formatDuration())
+			a.current -= time.Second
+			if !a.nextInerm {
+				putText(a.nextSession(), positionButtom, termbox.ColorLightBlue+termbox.AttrBold)
+			} else {
+				putText(a.currSession(), positionButtom, termbox.ColorYellow+termbox.AttrBold)
 			}
+			flush()
+			if a.current == 0 {
+				if a.nextInerm {
+					a.current = a.intermission
+				} else {
+					a.current = a.timmer
+					a.session++
+					clearT()
+					putText("Press any key to continue or q to quit...", positionMiddle, termbox.ColorLightRed+termbox.AttrBold)
+					flush()
+					if ev := <-a.queues; isQuit(ev) {
+						break loop
+					}
 
-			termbox.Flush()
+				}
+				a.nextInerm = !a.nextInerm
+			}
 		}
-
 	}
 
+}
+
+func isQuit(ev termbox.Event) bool {
+	return ev.Ch == 'q' || ev.Ch == 'Q' || ev.Key == termbox.KeyEsc || ev.Key == termbox.KeyCtrlC
 }
 
 /*
--- ui.go
+++ ui.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"unicode/utf8"
+
 	"github.com/nsf/termbox-go"
 )
 
@@ -23,16 +25,44 @@ func putTime(s string) {
 	}
 }
 
-func putPaused() {
+func putMsg(m symbol, color termbox.Attribute) {
 	x, y := termbox.Size()
-	x, y = x/2-pausedText.width()/2, y*3/4
+	x, y = x/2-m.width()/2, y*3/4
 	lx := x
-	for _, l := range pausedText {
+	for _, l := range m {
 		for _, r := range l {
-			termbox.SetCell(x, y, r, termbox.ColorRed, termbox.ColorDefault)
+			termbox.SetCell(x, y, r, color, termbox.ColorDefault)
 			x++
 		}
 		y++
 		x = lx
+	}
+}
+
+type position uint8
+
+const (
+	positionTop = iota
+	positionMiddle
+	positionButtom
+)
+
+func putText(t string, p position, color termbox.Attribute) {
+	x, y := termbox.Size()
+	x = x/2 - utf8.RuneCountInString(t)/2
+	switch p {
+	case positionTop:
+		y /= 7
+	case positionMiddle:
+		y /= 2
+	case positionButtom:
+		y = y * 3 / 4
+	default:
+		return
+	}
+
+	for _, r := range t {
+		termbox.SetCell(x, y, r, color, termbox.ColorDefault)
+		x++
 	}
 }
